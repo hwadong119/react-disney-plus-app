6169ffe31c73debcd5eb59201291cfb6f36f386b
hwadong119 <jeonghwayun0119@naver.com>
2023-09-16 15:53:49 +0900

    슬라이드 기능 추가

--- src/components/Row.js
+++ src/components/Row.js
@@ -20,7 +20,11 @@ const Row = ({ title, id, fetchUrl }) => {
       <h2>{title}</h2>
       <div className='slider'>
         <div className='slider__arrow-left'>
-          <span className='arrow'>
+          <span className='arrow'
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80
+            }}
+          >
             {"<"}
           </span>
         </div>
@@ -35,7 +39,11 @@ const Row = ({ title, id, fetchUrl }) => {
           ))}
         </div>
         <div className='slider__arrow-right'>
-          <span className='arrow'>
+          <span className='arrow'
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80
+            }}
+          >
             {">"}
           </span>
         </div>
